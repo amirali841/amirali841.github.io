9e6a720679850ada2fc5c4986afb2a8853e4b32f
test <amiralihskh@gamil.com>
2024-07-19 17:29:10 +0330

    Core Ui

---
+++ chunk-CSYFJD3Y.js
@@ -0,0 +1,1 @@
+import"./chunk-2QKSPLSF.js";var o=[{path:"",data:{title:"Items"},children:[{path:"",redirectTo:"colors",pathMatch:"full"},{path:"Item-1",loadComponent:()=>import("./chunk-UMMKUH3G.js").then(t=>t.ColorsComponent),data:{title:"Item 1"}},{path:"Item-2",loadComponent:()=>import("./chunk-UILVEPOI.js").then(t=>t.TypographyComponent),data:{title:"Item 2"}}]}];export{o as routes};
---
+++ chunk-GTZTEQNY.js
@@ -0,0 +1,1 @@
+import"./chunk-2QKSPLSF.js";var t=[{path:"",loadComponent:()=>import("./chunk-AXNTDCIT.js").then(o=>o.DashboardComponent),data:{title:$localize`Dashboard`}}];export{t as routes};
